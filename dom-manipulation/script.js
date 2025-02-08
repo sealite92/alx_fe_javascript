875d97b4d9701db3c1014ade7c950e4fe0848a90
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-08 18:33:40 +0100

    added the html and javascript files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -12,5 +12,25 @@ function displayRandomQuotes() {
 const newQuoteBtn = document
   .getElementById("newQuote")
   .addEventListener("click", displayRandomQuotes);
-function showRandomQuote() {}
 function createAddQuoteForm() {}
+
+const newQuoteText = document.getElementById("newQuoteText");
+const newQuoteCategory = document.getElementById("newQuoteCategory");
+
+function addQuote() {
+const category = newQuoteCategory.value.trim() 
+const text = newQuoteText.value.trim() 
+
+if (!category || !text) {
+  alert( "enter text and category"
+} else {
+  const newQuote = {
+    category: category,
+    text : text
+  }
+}
+quotes.push(newQuote)
+
+newQuoteCategory.value = "";
+      newQuoteText.value = "";
+}
