100aa4e26301b164f4295d179f27a778a654b6d4
Nnalue Fabian Izuchukwu <nnaluefabian@gmail.com>
2025-02-08 17:59:05 +0100

    added the html and javascript files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,6 +3,14 @@ const quotes = [
     category: "text",
   },
 ];
+function displayRandomQuotes() {
+  const randomQuote = Math.floor(Math.random() * quotes.length);
 
+  const addNewQuotes = document.getElementById("quoteDisplay");
+  addNewQuotes.innerHTML = quotes[randomQuote];
+}
+const newQuoteBtn = document
+  .getElementById("newQuote")
+  .addEventListener("click", displayRandomQuotes);
 function showRandomQuote() {}
 function createAddQuoteForm() {}
